29ba74aa59856694095c90de7163e10ad5ba1aea
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-01-24 17:06:56 +0100

    first commit

---
+++ snack 1/js/main.js
@@ -0,0 +1,23 @@
+const vips = [
+    'Dwayne Johnson',
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin',
+  ];
+
+const listVip = vips.map((vip, numeroTavolo) => {
+      let listVip = {
+          nomeTavolo: 'tavoloVip',
+          vip: vip,
+          numeroTavolo: numeroTavolo + 1
+      };
+      return listVip;
+  });
+  console.table(vips);
